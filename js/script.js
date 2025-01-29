7a2a1cbe017aa221433458e7b47c2e03d93042e3
RijalPratama21 <rijal.pratama_ti23@nusaputra.ac.id>
2025-01-29 14:29:57 +0700

    mengupgrade file js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-// humberger menu 
+// Mendapatkan elemen-elemen yang dibutuhkan
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const menuList = document.getElementById("menu-list");
+const body = document.getElementsByTagName('BODY')[0];
 
-// ini perumpamaan, ketika kita menklik tombol hamburger menu kita mau akan terjadi apa.
-hamburgerMenu.addEventListener("click", () => {
-    menuList.classList.toggle("hidden");
+// Menangani klik pada hamburger menu
+hamburgerMenu.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevent klik pada hamburger menu agar tidak memicu event listener di luar
+    menuList.classList.toggle("hidden"); // Toggle navbar
+});
+
+// Menangani klik di luar navbar (untuk menutup menu)
+body.addEventListener("click", (event) => {
+    if (!menuList.contains(event.target) && event.target !== hamburgerMenu) {
+        menuList.classList.add("hidden"); // Menyembunyikan navbar jika klik di luar
+    }
 });
